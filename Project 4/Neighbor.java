afea4240bce40846205b5897b855b5f1ac4ca145
Brendan Drusda <bmd66@pitt.edu>
2018-04-04 18:43:20 -0400

    Adds the first edge length correctly

--- Project 4/Neighbor.java
+++ Project 4/Neighbor.java
@@ -1,37 +1,48 @@
 public class Neighbor
 {
     //Neighbor node to keep track of a a vertex's neighbors in the adjacency list
-    private int vertex;//The vertex that is a neighbor
+    //If we want to get the edge's information, we will look up vertex a in the adjacency list and the edge is stored with the corresponding b
+    private Edge edge;      //The edge between the vertex at the index and the vertex in this neighbor
+    private int vertex;     //The vertex that is a neighbor
     private Neighbor next;  //The next neighbor
+
 /*Constrctors*/
     public Neighbor()
     {
+        edge = null;
         vertex = -1;
         next = null;
     }
 
-    public Neighbor(int i)
+    public Neighbor(Edge i)
     {
-        vertex = i;
+        edge = i;
+        vertex = i.getB();
         next = null;
     }
 
 /*Setters*/
-    public void addNeighbor(int i)
+    public void addNeighbor(Edge i, char to)
     {
         if(vertex == -1)    /*If this vertex isn't initialized*/
         {
-            vertex = i;         /*Set this neighbor equal to the vertex*/
+            edge = i;
+            if(to == 'a')
+                vertex = i.getA();          /*Set this neighbor equal to the vertex*/
+            else
+                vertex = i.getB();          /*Set this neighbor equal to the vertex*/
         }
         else                /*If this node has a vertex*/
         {
             if(next == null)    /*If next is not initialized*/
             {
                 next = new Neighbor(i); /*Set next equal to the neighbor*/
+                if(to == 'a')           /*If we wanted to go to vertex a instead of b, this is set incorrectly now*/
+                    next.setVertex(i.getA());   /*So set the vertex to the correct value*/
             }
             else                /*If next exists*/
             {
-                next.addNeighbor(i);    /*Go to it and recursively addNeighbor*/
+                next.addNeighbor(i, to);    /*Go to it and recursively addNeighbor*/
             }
         }
     }
@@ -42,6 +53,11 @@ public class Neighbor
     }
 
 /*Getters*/
+    public Edge getEdge()
+    {
+        return edge;
+    }
+
     public int getVertex()
     {
         return vertex;
--- Project 4/NetworkAnalysis.java
+++ Project 4/NetworkAnalysis.java
@@ -41,36 +41,115 @@ public class NetworkAnalysis
             temp.setBandwidth(Integer.parseInt(theEdge[3]));    //Set the bandwidth
             temp.setLength(Integer.parseInt(theEdge[4]));       //Set the length
 
-            /*It REALLY feels like we should be storing these complete edges somewhere, but idk where*/
-                /*Maybe we store the edges in the neighbors?  The array index marks a and the vertex in neighbor is b*/
-            adjList[temp.getA()].addNeighbor(temp.getB());      //Mark B as a neighbor of A
-            adjList[temp.getB()].addNeighbor(temp.getA());      //Mark A as a neighbor of B
-    //This will allow duplicates, but only if the list given to us has duplicates
+            adjList[temp.getA()].addNeighbor(temp, 'b');             //Mark B as a neighbor of A
+            adjList[temp.getB()].addNeighbor(temp, 'a');              //Mark A as a neighbor of B
         }
+//        for(int i = 0; i < adjList.length; i++)                 //Initialize the adjacency list
+//            System.out.println(adjList[i]);
+/*Graph is fully loaded into the adjacency list*/
 
-        //After loading the graph
-        //Present user with options
-            //1. Find the lowest latency path between any two points
+        in = new Scanner(System.in);
+        while(true)
+        {
+            /*Present user with options*/
+            System.out.println("Would you like to:\n"+
+                               " 1. Find the lowest latency path between any two points\n"+
+                               " 2. Determine whether or not the graph is copper-only connected\n"+
+                               " 3. Find the maximum amount of data that can be transmitted from one vertex to another\n"+
+                               " 4. Find the minimum average latency spanning tree\n"+
+                               " 5. Determine whether the graph would remain connected even if any two vertices fail\n"+
+                               " 6. Quit the program\n");
+            int response = -1;
+            while(response > 6 || response < 1)
+            {
+                response = in.nextInt();
+            }
+            if(response == 1)           //1. Find the lowest latency path between any two points
+            {
                 //Give the bandwidth available along that path
-                //To do this:
-                    //1. Prompt the user for the two vertices
-                    //2. Output the sequence of vertices that comprise the lowest-latency path, in order from a to b
-                        //Must find the path between these that will take the least amount of time for a single packet to travel
-                            //Assume that time required to travel is the sum of the times required to travel each link
-                    //3. Output the bandwidth thtat is available along the resulting path
-                        //aka the minimum bandwidth of all the edges in the path
-
-            //2. Determine whether or not the graph is copper-only connected
-                //aka connected when only considered copper links (ignore the fiber-optic)
-            //3. Find the maximum amount of data that can be transmitted from one vertex to another
                 //Prompt the user for the two vertices
+                System.out.print("Enter any two vertices from "+0+" to "+(numVertices-1)+": ");
+                int a = in.nextInt();
+                int b = in.nextInt();
+
+                lowest_latency(adjList, numVertices, a, b);
+            }
+            else if(response == 2)      //2. Determine whether or not the graph is copper-only connected
+            {
+                //aka connected when only considered copper links (ignore the fiber-optic)
+            }
+            else if(response == 3)      //3. Find the maximum amount of data that can be transmitted from one vertex to another
+            {
+                //Prompt the user for the two vertices
+                System.out.print("Enter any two vertices from "+0+" to "+(numVertices-1)+": ");
+                int a = in.nextInt();
+                int b = in.nextInt();
                 //Output the maximum amount of data the can be transmitted from a to b
-            //4. Find the minimum average latency spanning tree
+            }
+            else if(response == 4)      //4. Find the minimum average latency spanning tree
+            {
                 //aka the spanning tree with the lowest average latency per edge
-            //5. Determine whether the graph would remain connected if any two vertices fail
+            }
+            else if(response == 5)      //5. Determine whether the graph would remain connected if any two vertices fail
+            {
                 //You are not prompting the user for two vertices that could fail
                 //This is whether any pair of vertices that, should they both fail, would cause the graph to become disconnected
                     //Do we just want to determine the min cut and see if it's two or less?
-            //6. Quit the program
+            }
+            else                        //6. Quit the program
+            {
+                System.out.println("Exiting program");
+                System.exit(0);
+            }
+        }
+    }
+
+    public static void lowest_latency(Neighbor[] adjList, int numVertices, int a, int b)
+    {
+        boolean reached = false;
+        //We're going to want to use Dijkstra's for this
+        double distTo[] = new double[numVertices];    //Create distance array corresponding to the distance from a to each vertex
+        for(int i = 0; i < distTo.length; i++)  //Set the distance to MAX_INT initially
+            distTo[i] = Integer.MAX_VALUE;
+
+        Neighbor curr = adjList[a];             //Set curr = a
+        Edge currEdge = curr.getEdge();
+        distTo[currEdge.getA()] = 0;                //Distance is itself
+
+        while(!reached)                         //While b is not yet reached
+        {
+            while(curr != null)                 //Check all edges in the neighbor
+            {
+                currEdge = curr.getEdge();          //Get the edge of the current vertex
+                if(distTo[currEdge.getB()] == Integer.MAX_VALUE)    //The time calculation has not yet begun
+                {
+/*Replace all lengths with travel time once debugging is done*/
+                    //distTo[currEdge.getB()] = currEdge.getTravelTime();    //Compute tentative time from start to the unvisited neighbor through curr
+                    distTo[currEdge.getB()] = currEdge.getLength();    //Compute tentative time from start to the unvisited neighbor through curr
+                }
+                else
+                {
+                    //distTo[currEdge.getB()] += currEdge.getTravelTime();   //Add this time to the total time
+                    distTo[currEdge.getB()] += currEdge.getLength();   //Add this time to the total time
+                }
+                curr = curr.getNext();
+
+                for(int i = 0; i < distTo.length; i++)
+                {
+                    System.out.println("Vertex "+i+": "+distTo[i]);
+                }
+                System.out.println();
+            }
+
+                //Update any vertices for which a  lesser distance is computer
+            //Mark curr as unvisited
+            //Let curr be the unvisited vertex with the smallest tentative distance from start
+        }
+
+        //Output the sequence of vertices that comprise the lowest-latency path, in order from a to b
+            //Must find the path between these that will take the least amount of time for a single packet to travel
+                //Assume that time required to travel is the sum of the times required to travel each link
+        //Output the bandwidth thtat is available along the resulting path
+                //aka the minimum bandwidth of all the edges in the path
     }
 }
